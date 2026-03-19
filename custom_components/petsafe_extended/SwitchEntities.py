086bfa2eb926a9abe5209d5d3caf8e11a09af923
Thomas Wright <thomas.h.f.wright@gmail.com>
2026-03-19 18:13:53 +0000

    Prep to load in blueprint

---
+++ custom_components/petsafe_extended/SwitchEntities.py
@@ -0,0 +1,149 @@
+from typing import Any
+
+from homeassistant.components.switch import SwitchEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+import petsafe
+
+from . import PetSafeCoordinator, PetSafeData
+from .const import DOMAIN, FEEDER_MODEL_GEN1, MANUFACTURER
+
+
+class PetSafeSwitchEntity(CoordinatorEntity, SwitchEntity):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        api_name: str,
+        name: str,
+        coordinator: PetSafeCoordinator,
+        device_type: str,
+        icon: str = None,
+        device_class: str = None,
+        entity_category: str = None,
+    ):
+        super().__init__(coordinator)
+        self._attr_name = name
+        self._attr_device_class = device_class
+        self._attr_has_entity_name = True
+        self._coordinator = coordinator
+        self._api_name = api_name
+        self._attr_should_poll = True
+        self._attr_unique_id = api_name + "_" + device_type
+        self._attr_icon = icon
+        self._device_type = device_type
+        self._attr_entity_category = entity_category
+
+
+class PetSafeLitterboxSwitchEntity(PetSafeSwitchEntity):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        name: str,
+        coordinator: PetSafeCoordinator,
+        device_type: str,
+        device: petsafe.devices.DeviceScoopfree,
+        icon: str = None,
+        device_class: str = None,
+        entity_category: str = None,
+    ):
+        self._litterbox = device
+
+        super().__init__(
+            hass,
+            device.api_name,
+            name,
+            coordinator,
+            device_type,
+            icon,
+            device_class,
+            entity_category,
+        )
+
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, device.api_name)},
+            manufacturer=MANUFACTURER,
+            name=device.friendly_name,
+            model=device.product_name,
+            sw_version=device.firmware,
+        )
+
+    def _handle_coordinator_update(self) -> None:
+        data: PetSafeData = self.coordinator.data
+        litterbox: petsafe.devices.DeviceScoopfree = next(
+            x for x in data.litterboxes if x.api_name == self._api_name
+        )
+
+        self.async_write_ha_state()
+        return super()._handle_coordinator_update()
+
+
+class PetSafeFeederSwitchEntity(PetSafeSwitchEntity):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        name: str,
+        coordinator: PetSafeCoordinator,
+        device_type: str,
+        device: petsafe.devices.DeviceSmartFeed,
+        icon: str = None,
+        device_class: str = None,
+        entity_category: str = None,
+    ):
+        self._feeder = device
+
+        super().__init__(
+            hass,
+            device.api_name,
+            name,
+            coordinator,
+            device_type,
+            icon,
+            device_class,
+            entity_category,
+        )
+
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, device.api_name)},
+            manufacturer=MANUFACTURER,
+            name=device.friendly_name,
+            sw_version=device.firmware,
+            # NB: Gen1 smart feeders do not report a product_name
+            model=device.product_name or FEEDER_MODEL_GEN1,
+        )
+        self._device = device
+
+    def _handle_coordinator_update(self) -> None:
+        data: PetSafeData = self.coordinator.data
+        feeder: petsafe.devices.DeviceSmartFeed = next(
+            x for x in data.feeders if x.api_name == self._api_name
+        )
+        if self._device_type == "child_lock":
+            self._attr_is_on = feeder.is_locked
+        elif self._device_type == "feeding_paused":
+            self._attr_is_on = feeder.is_paused
+        elif self._device_type == "slow_feed":
+            self._attr_is_on = feeder.is_slow_feed
+
+        self.schedule_update_ha_state(True)
+        return super()._handle_coordinator_update()
+
+    async def async_update(self) -> None:
+        return await super().async_update()
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        if self._device_type == "child_lock":
+            await self._device.lock(True)
+        elif self._device_type == "feeding_paused":
+            await self._device.pause(True)
+        elif self._device_type == "slow_feed":
+            await self._device.slow_feed(True)
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        if self._device_type == "child_lock":
+            await self._device.lock(False)
+        elif self._device_type == "feeding_paused":
+            await self._device.pause(False)
+        elif self._device_type == "slow_feed":
+            await self._device.slow_feed(False)
---
+++ custom_components/petsafe_extended/__init__.py
@@ -0,0 +1,309 @@
+"""The PetSafe Integration integration."""
+
+from __future__ import annotations
+
+import asyncio
+import logging
+from datetime import timedelta
+
+import httpx
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    ATTR_AREA_ID,
+    ATTR_DEVICE_ID,
+    ATTR_ENTITY_ID,
+    CONF_ACCESS_TOKEN,
+    CONF_EMAIL,
+    CONF_TOKEN,
+    Platform,
+)
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.helpers.httpx_client import get_async_client
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+import petsafe
+
+from .const import (
+    ATTR_AMOUNT,
+    ATTR_SLOW_FEED,
+    ATTR_TIME,
+    CONF_REFRESH_TOKEN,
+    DOMAIN,
+    SERVICE_ADD_SCHEDULE,
+    SERVICE_DELETE_ALL_SCHEDULES,
+    SERVICE_DELETE_SCHEDULE,
+    SERVICE_FEED,
+    SERVICE_MODIFY_SCHEDULE,
+    SERVICE_PRIME,
+)
+from .helpers import get_feeders_for_service
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS: list[Platform] = [
+    Platform.SENSOR,
+    Platform.SWITCH,
+    Platform.BUTTON,
+    Platform.SELECT,
+    Platform.LOCK,
+]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up PetSafe Integration from a config entry."""
+    client = petsafe.PetSafeClient(
+        entry.data.get(CONF_EMAIL),
+        entry.data.get(CONF_TOKEN),
+        entry.data.get(CONF_REFRESH_TOKEN),
+        entry.data.get(CONF_ACCESS_TOKEN),
+        client = get_async_client(hass)
+    )
+
+    hass.data.setdefault(DOMAIN, {})
+
+    coordinator = PetSafeCoordinator(hass, client, entry)
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    async def handle_add_schedule(call: ServiceCall) -> None:
+        device_ids = call.data.get(ATTR_DEVICE_ID)
+        area_ids = call.data.get(ATTR_AREA_ID)
+        entity_ids = call.data.get(ATTR_ENTITY_ID)
+        time = call.data.get(ATTR_TIME)
+        amount = call.data.get(ATTR_AMOUNT)
+        matched_devices = get_feeders_for_service(
+            hass, area_ids, device_ids, entity_ids
+        )
+        for device_id in matched_devices:
+            device = next(
+                d for d in await coordinator.get_feeders() if d.api_name == device_id
+            )
+            if device is not None:
+                await device.schedule_feed(time, amount, False)
+
+    hass.services.async_register(DOMAIN, SERVICE_ADD_SCHEDULE, handle_add_schedule)
+
+    async def handle_delete_schedule(call: ServiceCall) -> None:
+        device_ids = call.data.get(ATTR_DEVICE_ID)
+        area_ids = call.data.get(ATTR_AREA_ID)
+        entity_ids = call.data.get(ATTR_ENTITY_ID)
+        time = call.data.get(ATTR_TIME)
+        matched_devices = get_feeders_for_service(
+            hass, area_ids, device_ids, entity_ids
+        )
+
+        for device_id in matched_devices:
+            device = next(
+                d for d in await coordinator.get_feeders() if d.api_name == device_id
+            )
+            if device is not None:
+                schedules = await device.get_schedules()
+                for schedule in schedules:
+                    if schedule["time"] + ":00" == time:
+                        await device.delete_schedule(str(schedule["id"]), False)
+                        break
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_DELETE_SCHEDULE, handle_delete_schedule
+    )
+
+    async def handle_delete_all_schedules(call: ServiceCall) -> None:
+        device_ids = call.data.get(ATTR_DEVICE_ID)
+        area_ids = call.data.get(ATTR_AREA_ID)
+        entity_ids = call.data.get(ATTR_ENTITY_ID)
+        matched_devices = get_feeders_for_service(
+            hass, area_ids, device_ids, entity_ids
+        )
+
+        for device_id in matched_devices:
+            device = next(
+                d for d in await coordinator.get_feeders() if d.api_name == device_id
+            )
+            if device is not None:
+                await device.delete_all_schedules(False)
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_DELETE_ALL_SCHEDULES, handle_delete_all_schedules
+    )
+
+    async def handle_modify_schedule(call: ServiceCall) -> None:
+        device_ids = call.data.get(ATTR_DEVICE_ID)
+        area_ids = call.data.get(ATTR_AREA_ID)
+        entity_ids = call.data.get(ATTR_ENTITY_ID)
+        time = call.data.get(ATTR_TIME)
+        amount = call.data.get(ATTR_AMOUNT)
+        matched_devices = get_feeders_for_service(
+            hass, area_ids, device_ids, entity_ids
+        )
+
+        for device_id in matched_devices:
+            device = next(
+                d for d in await coordinator.get_feeders() if d.api_name == device_id
+            )
+            if device is not None:
+                schedules = await device.get_schedules()
+                for schedule in schedules:
+                    if schedule["time"] + ":00" == time:
+                        await device.modify_schedule(
+                            schedule["time"], amount, str(schedule["id"]), False
+                        )
+                        break
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_MODIFY_SCHEDULE, handle_modify_schedule
+    )
+
+    async def handle_feed(call: ServiceCall) -> None:
+        device_ids = call.data.get(ATTR_DEVICE_ID)
+        area_ids = call.data.get(ATTR_AREA_ID)
+        entity_ids = call.data.get(ATTR_ENTITY_ID)
+        amount = call.data.get(ATTR_AMOUNT)
+        slow_feed = call.data.get(ATTR_SLOW_FEED)
+        matched_devices = get_feeders_for_service(
+            hass, area_ids, device_ids, entity_ids
+        )
+
+        for device_id in matched_devices:
+            device = next(
+                d for d in await coordinator.get_feeders() if d.api_name == device_id
+            )
+            if device is not None:
+                await device.feed(amount, slow_feed, False)
+                await coordinator.async_request_refresh()
+
+    hass.services.async_register(DOMAIN, SERVICE_FEED, handle_feed)
+
+    async def handle_prime(call: ServiceCall) -> None:
+        device_ids = call.data.get(ATTR_DEVICE_ID)
+        area_ids = call.data.get(ATTR_AREA_ID)
+        entity_ids = call.data.get(ATTR_ENTITY_ID)
+        matched_devices = get_feeders_for_service(
+            hass, area_ids, device_ids, entity_ids
+        )
+
+        for device_id in matched_devices:
+            device = next(
+                d for d in await coordinator.get_feeders() if d.api_name == device_id
+            )
+            if device is not None:
+                # NB: DeviceSmartFeed.prime() synchronously updates state after priming.
+                # Directly send a 5/8 cup meal here so that we can defer the update.
+                await device.feed(5, False, False)
+                await coordinator.async_request_refresh()
+
+    hass.services.async_register(DOMAIN, SERVICE_PRIME, handle_prime)
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await coordinator.async_config_entry_first_refresh()
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+class PetSafeData:
+    def __init__(
+        self,
+        feeders: list[petsafe.devices.DeviceSmartFeed],
+        litterboxes: list[petsafe.devices.DeviceScoopfree],
+        smartdoors: list[petsafe.devices.DeviceSmartDoor],
+    ):
+        self.feeders = feeders
+        self.litterboxes = litterboxes
+        self.smartdoors = smartdoors
+
+
+class PetSafeCoordinator(DataUpdateCoordinator):
+    """Data Update Coordinator for petsafe devices."""
+
+    def __init__(
+        self, hass: HomeAssistant, api: petsafe.PetSafeClient, entry: ConfigEntry
+    ):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="PetSafe",
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=30),
+        )
+        self.api: petsafe.PetSafeClient = api
+        self.hass: HomeAssistant = hass
+        self._feeders: list[petsafe.devices.DeviceSmartFeed] = None
+        self._litterboxes: list[petsafe.devices.DeviceScoopfree] = None
+        self._smartdoors: list[petsafe.devices.DeviceSmartDoor] = None
+        self._device_lock = asyncio.Lock()
+        self.entry = entry
+        self._authErrorCount = 0
+
+    async def get_feeders(self) -> list[petsafe.devices.DeviceSmartFeed]:
+        """Return the list of feeders."""
+        async with self._device_lock:
+            try:
+                if self._feeders is None:
+                    self._feeders = await self.api.get_feeders()
+            except httpx.HTTPStatusError as ex:
+                if ex.response.status_code in (401, 403):
+                    await self.entry.async_start_reauth(self.hass)
+                else:
+                    raise
+            return self._feeders
+
+    async def get_litterboxes(self) -> list[petsafe.devices.DeviceScoopfree]:
+        """Return the list of litterboxes."""
+        async with self._device_lock:
+            try:
+                if self._litterboxes is None:
+                    self._litterboxes = await self.api.get_litterboxes()
+            except httpx.HTTPStatusError as ex:
+                if ex.response.status_code in (401, 403):
+                    await self.entry.async_start_reauth(self.hass)
+                else:
+                    raise
+            return self._litterboxes
+
+    async def get_smartdoors(self) -> list[petsafe.devices.DeviceSmartDoor]:
+        """Return the list of smart doors."""
+        async with self._device_lock:
+            try:
+                if self._smartdoors is None:
+                    self._smartdoors = await self.api.get_smartdoors()
+            except httpx.HTTPStatusError as ex:
+                if ex.response.status_code in (401, 403):
+                    await self.entry.async_start_reauth(self.hass)
+                else:
+                    raise
+            return self._smartdoors
+
+    async def _async_update_data(self) -> PetSafeData:
+        """Fetch data from API endpoint."""
+        try:
+            async with self._device_lock:
+                self._feeders = await self.api.get_feeders()
+                self._litterboxes = await self.api.get_litterboxes()
+                self._smartdoors = await self.api.get_smartdoors()
+                self._authErrorCount = 0
+                return PetSafeData(
+                    self._feeders,
+                    self._litterboxes,
+                    self._smartdoors,
+                )
+        except httpx.HTTPStatusError as ex:
+            if ex.response.status_code in (401, 403):
+                self._authErrorCount += 1
+                if self._authErrorCount >= 5:
+                    self._authErrorCount = 0
+                    raise ConfigEntryAuthFailed() from ex
+
+            else:
+                raise UpdateFailed() from ex
+        except Exception as ex:
+            raise UpdateFailed() from ex
